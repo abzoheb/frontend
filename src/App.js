ac4a96ed7691eb780cdc5d3a27b2239e2c6bd279
abzoheb <ab.zoheb@gmail.com>
2023-08-23 23:37:23 +0530

    frontend2

--- src/App.js
+++ src/App.js
@@ -4,26 +4,51 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div id="mySidebar" class="sidebar">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
+      </div>
+      <div id="main">
+        <button class="openbtn" onclick="openNav()">&#9776; MENU</button>
+      </div>
+      <div></div>
       <div className='column'>
         <div>
-          <img src='./images/dog.png' alt='dog' />
+          <img src='./images/Exercise.png' alt='dog' />
+          <button className='index_button' ><div><b>EXERCISE</b></div></button>
         </div>
         <div>
-          <img src='./images/read.png' alt='read' />
+          <img src='./images/healthy_diet.png' alt='read' />
+          <button className='index_button'><div><b>HEALTHY DIET</b></div></button>
         </div>
         <div>
-          <img src='./images/eat.png' alt='eat' />
+          <img src='./images/learning.png' alt='eat' />
+          <button className='index_button'><div><b>LEARNING</b></div></button>
         </div>
       </div>
       <div className='column'>
         <div>
-          <img src='./images/run.png' alt='run' />
+          <img src='./images/meditation.png' alt='run' />
+          <button className='index_button'><div><b>MEDITATION</b></div></button>
         </div>
         <div>
-          <img src='./images/smoke.png' alt='smoke' />
+          <img src='./images/sleep.png' alt='smoke' />
+          <button className='index_button' ><div><b>SLEEP</b></div></button>
         </div>
         <div>
-          <img src='./images/teeth.png' alt='teeth' />
+          <img src='./images/water.png' alt='teeth' />
+          <button className='index_button'><div><b>WATER</b></div></button>
+        </div>
+        <div>
+          <img src='./images/hygiene.png' alt='zo' />
+          <button className='index_button'><div><b>HYGIENE</b></div></button>
+        </div>
+        <div>
+          {/* <img src='' alt='sd'/> 
+          <b></b> */}
         </div>
       </div>
     </div>
